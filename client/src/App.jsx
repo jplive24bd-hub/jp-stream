ba29c55c03655208d746480659bf4d6c73cbbaf7
Md Juwel Raj <jp.live24bd@gmail.com>
2026-09-23 20:36:44 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,13 @@
-import React from "react";
-import { Switch, Route } from "wouter";
-import Home from "./pages/Home";
-import Admin from "./pages/Admin";
+import React from 'react';
+import { Switch, Route } from 'wouter';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
 
 export default function App() {
   return (
     <Switch>
-      {/* অ্যাপ ব্যবহারকারীদের জন্য সরাসরি হোম পেজ (কোনো লগইন নেই) */}
       <Route path="/" component={Home} />
-      
-      {/* শুধুমাত্র ব্রাউজার/ইউআরএল দিয়ে অ্যাক্সেস করার জন্য এডমিন পেজ */}
       <Route path="/admin" component={Admin} />
-      
-      {/* অন্য যেকোনো রুটে সরাসরি হোম পেজ দেখাবে */}
       <Route component={Home} />
     </Switch>
   );
